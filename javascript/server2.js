57620e35cfedd6dfb626817977822d3f5780ab80
Kevin <kjh7796@gmail.com>
2018-04-02 00:08:46 -0400

    Fully working

--- javascript/server2.js
+++ javascript/server2.js
@@ -2,37 +2,48 @@ const snoowrap = require('snoowrap')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const info = require('dotenv').config()
+const dotenv = require('dotenv').config()
+
 
 const r = new snoowrap({
   userAgent: "Jarmahent Random Image Catcher",
   clientId: process.env.id,
   clientSecret: process.env.secret,
-  username: process.env.username,
+  username: process.env.redditusername,
   password: process.env.password
-});
+})
 
-app.use(express.static('static'));
+app.use(express.static('static'))
+
 app.use(function(req, res, next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + "/index.html");
-});
+  res.sendFile(__dirname + "/index.html")
+})
 
-app.post('/image', function(req, res){
-  var data = req.body;
-  console.log(data);
-});
+app.post('/randomImage', function(req, res){
+  let subreddit = req.body.subreddit
+  r.getSubreddit(subreddit).getRandomSubmission().then(function(post){
+		if(post.url.includes(".jpg") || post.url.includes(".png")){
+			currentUrl = post.url
+			console.log(currentUrl)
+			res.send({'url': currentUrl})
+			res.end()
+		} else{
+			res.send({"url":currentUrl}) //Sad face
+      res.end()
+		}
+  }).catch(function(error){res.status(500).send({'error': error})});
+})
 
-var server = app.listen(8081, function(){
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log("App listening at: https://%s:%s", host, port);
+let server = app.listen(8081, function(){
+  let host = server.address().address
+  let port = server.address().port
+  console.log("App listening at: https://%s:%s", host, port)
 })
